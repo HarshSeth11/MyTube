07f90c4313f7322209ddd4771289007b527424bf
HarshSeth11 <harshsethr@gmail.com>
2024-02-13 14:40:32 +0530

    Wrote dashboard, healthCheck, comment Controller

--- backend/src/controllers/comment.controller.js
+++ backend/src/controllers/comment.controller.js
@@ -8,18 +8,106 @@ const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
     const { videoId } = req.params;
     const { page = 1, limit = 10 } = req.query;
+
+    const comments = await Comment.aggregate([
+        {
+            $match: {
+                videoId
+            }
+        },
+        {
+            $skip: (page-1) * limit
+        },
+        {
+            $limit: limit
+        }
+    ]);
+
+    res
+    .status(200)
+    .json(
+        new ApiResponse(200, comments.length > 0? "Comments fetched successfully" : "There are not comments on this post right now", comments.length > 0? comments : {})
+    );
 });
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
+    // Destructure the content
+    // get the videoId from the parameters
+    const { videoId } = req.params;
+    const { content } = req.body;
+
+    if(content.trim() === "" || content === undefined || content === null) {
+        throw new ApiError(400, "Content is required.")
+    };
+
+    const comment = await Comment.create({
+        content,
+        video: videoId,
+        owner: req.user._id
+    });
+
+    if(!comment) {
+        throw new ApiError(500, "There is some Problem while adding the comment at the server end.");
+    }
+
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(201, "Comment added Successfully", comment)
+    );
 });
 
 const updateComment = asyncHandler(async (req, res) => {
     // TODO: update a comment
+    // get the Comment Id from the parameters
+    // get the updated content
+    // check if the comment is updated by same person who added it otherwise anybody could add the comment.
+
+    const { commentId } = req.params;
+    const { content } = req.body;
+
+    if(content.trim() === "" || content === undefined || content === null) {
+        throw new ApiError(400, "Content is required.")
+    };
+
+    const newComment = await Comment.findOneAndUpdate({
+        _id: commentId, 
+        owner: req.user._id
+    },
+    {
+        content
+    },
+    {
+        new: true
+    });
+
+    if(!newComment) {
+        throw new ApiError(500, "There is some Problem while Updating the comment at the server end.");
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, "Comment is updated Successfully", newComment)
+    );
 });
 
 const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
+    const { commentId } = req.params;
+
+    const comment = await Comment.findOneAndDelete({_id: commentId, owner: req.user._id});
+
+    if(!comment) {
+        throw new ApiError(500, "There was not comment to delete.");
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, "Comment deleted Successfully", comment)
+    );
 });
 
 export { getVideoComments, addComment, updateComment, deleteComment };
--- backend/src/controllers/dashboard.controller.js
+++ backend/src/controllers/dashboard.controller.js
@@ -8,10 +8,123 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+    const firstInfo = await Video.aggregate([
+        {
+            $match: {
+                owner: req.user._id
+            }
+        },
+        {
+            $group: {
+                _id: null,
+                totalViews: {
+                    $add: "$views"
+                },
+                totalVideos: {
+                    $add: 1
+                }
+            }
+        },
+        {
+            $lookup: {
+                from: "likes",
+                localField: "_id",
+                foreignField: "video",
+                as: "videoLikes"
+            }
+        },
+        {
+            $addFields: {
+                totalLikes : {
+                    $size: "$videoLikes"
+                }
+            }
+        },
+        {
+            $group: {
+                _id: null,
+                totalChannelLikes: {
+                    $add: "$totalLikes"
+                },
+            }
+        },
+        {
+            $project: {
+                _id: 0,
+                totalVideos: 1,
+                totalViews: 1,
+                totalChannelLikes: 1
+            }
+        },
+    ]);
+
+    const totalSubscribers = await Subscription.aggregate([
+        {
+            $match: {
+                channel: req.user._id
+            }
+        },
+        {
+            $group: {
+                _id: null,
+                totalSubscribers: {
+                    $add: 1
+                }
+            }
+        },
+        {
+            $project: {
+                _id: 0,
+                totalSubscribers: 1
+            }
+        }
+    ]);
+
+    const totalChannelsSubscribedTo = await Subscription.aggregate([
+        {
+            $match: {
+                subscriber: req.user._id
+            }
+        },
+        {
+            $group: {
+                _id: null,
+                totalSubscribers: {
+                    $add: 1
+                }
+            }
+        },
+        {
+            $project: {
+                _id: 0,
+                totalSubscribers: 1
+            }
+        }
+    ]);
+
+
+    console.log(firstInfo, totalChannelsSubscribedTo, totalSubscribers)
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, "Channel Information Fetched Successfully", {firstInfo, totalChannelsSubscribedTo, totalSubscribers})
+    );
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
+    const allChannelVideos = await Video.find({owner: req.user._id});
+
+    if(!allChannelVideos) {
+        throw new ApiError(200, "There is not video on this channel");
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, "All Videos are fetched Successfully", allChannelVideos)
+    );
 })
 
 export {
