75aa900a926cbc9a02cc9c4f19f704271d00c6c6
HarshSeth11 <harshsethr@gmail.com>
2024-02-15 20:48:56 +0530

    Wrote Playlist Controller's Logic

--- backend/src/controllers/playlist.controller.js
+++ backend/src/controllers/playlist.controller.js
@@ -8,17 +8,113 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const createPlaylist = asyncHandler(async (req, res) => {
     const {name, description} = req.body
 
-    //TODO: create playlist
+    if([name, description].some((val) => val === null || val.trim() === "")) {
+        throw new ApiError(400, "Both name and description is required.");
+    }
+
+    if(!isValidObjectId(user.body._id)) {
+        throw new ApiError(401, "You are not authorized to create a playlist.");
+    }
+
+    const playlist = await Playlist.create({name, description, owner: req.user._id});
+
+    if(!playlist) {
+        throw new ApiError(500, "Error while creating playlist.");
+    }
+
+    return res
+    .status(400)
+    .json(
+        new ApiResponse(200, "Playlist is created", playlist)
+    );
 })
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
     const {userId} = req.params
     //TODO: get user playlists
+
+    if(!isValidObjectId(userId)) {
+        throw new ApiError(401, "There are no Playlists available");
+    }
+
+    // sending the information of first video along
+    const userPlaylists = await Playlist.aggregate([
+        {
+            $match: {
+                owner: userId
+            }
+        }, 
+        {
+            $lookup: {
+                from: "videos",
+                localField: "videos",
+                foreignField: "_id",
+                as: "firstVideoInfo"
+            },
+            pipeline: [
+                {
+                    $limit: 1
+                }
+            ]
+        },
+        {
+            $project: {
+                name: 1,
+                description: 1,
+                firstVideoInfo: 1,
+                owner: 1
+            }
+        }
+    ])
+
+    if(!userPlaylists) {
+        throw new ApiError(400, "There are no Playlists available")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, "Playlists are fetched successfully", userPlaylists)
+    );
 })
 
 const getPlaylistById = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
     //TODO: get playlist by id
+
+    const playlist = await Playlist.aggregate([
+        {
+            $match: {
+                _id: playlistId
+            }
+        }, 
+        {
+            $lookup: {
+                from: "videos",
+                localField: "videos",
+                foreignField: "_id",
+                as: "videos"
+            }
+        },
+        {
+            $project: {
+                name: 1,
+                description: 1,
+                videos: 1,
+                owner: 1
+            }
+        }
+    ])
+
+    if(!playlist) {
+        throw new ApiError(400, "This Playlist exist");
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, "Playlist fetched successfully", playlist)
+    );
 })
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
