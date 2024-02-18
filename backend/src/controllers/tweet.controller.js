c60bc4a521b8830c3c5c86ca78a37afe3aaddce0
HarshSeth11 <harshsethr@gmail.com>
2024-02-18 19:41:02 +0530

    Wrote Tweet Controller's Logic

--- backend/src/controllers/tweet.controller.js
+++ backend/src/controllers/tweet.controller.js
@@ -7,18 +7,75 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
     //TODO: create tweet
+    const { content } = req.body;
+
+    if(content.trim() === "") {
+        throw new ApiError(400, "Content is required");
+    }
+
+    const tweet = await Tweet.create({
+        content,
+        owner: mongoose.Types.ObjectId(req.user._id)
+    });
+
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(200, "Tweet is created", tweet)
+    );
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
+    
+    const tweets = await Tweet.find({owner: mongoose.Types.ObjectId(req.user._id)});
+
+    if(tweets.length === 0) {
+        throw new ApiError(404, "No tweet found");
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, "All Tweets are fetched successfully", tweets)
+    );
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
     //TODO: update tweet
+    const { tweetId } = req.params;
+    const { content } = req.body;
+    
+    const updatedTweet = await Tweet.findByIdAndUpdate(mongoose.Types.ObjectId(tweetId), {
+        content
+    }, {new : true});
+
+    if(!updatedTweet) {
+        throw new ApiError(404, "Tweet not found")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, "Tweet updated Successfully", updateTweet)
+    );
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
     //TODO: delete tweet
+    const { tweetId } = req.params;
+
+    const deletedTweet = await Tweet.findByIdAndDelete(tweetId);
+
+    if(!deletedTweet) {
+        throw new ApiError(404, "Tweet not found")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, "Tweet Deleted Successfully")
+    );
 })
 
 export {
